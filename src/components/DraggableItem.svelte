28a2324293f27e8b49f4335bb5269c176ad733fb
Cristian <cristian.gb.1995@hotmail.com>
2023-10-12 18:08:12 +0200

    The window component is almost ready!

---
+++ src/components/DraggableItem.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let left = 0;
+	export let top = 0;
+	let moving = false;
+	
+	const onMouseDown = () => moving = true;
+  const onMouseUp = () =>  moving = false;
+	const onMouseMove = (e: MouseEvent) => {
+		if (moving) {
+			left += e.movementX;
+			top += e.movementY;
+		}
+	}
+</script>
+
+<style>
+	.draggable {
+		user-select: none;
+    cursor: default;
+	}
+</style>
+
+<section
+  on:mousedown={onMouseDown}
+  class="draggable"
+  style="left: {left}px; top: {top}px;"
+  role="button"
+  tabindex="0"
+>
+	<slot />
+</section>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
--- src/components/Window.svelte
+++ src/components/Window.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
+  import DraggableItem from "@/components/DraggableItem.svelte";
   import { windowsHidden } from '@/stores';
   import type { WindowsHiddenType } from "@/stores";
   import { t } from "@/i18n";
@@ -10,41 +13,52 @@
   export let canBeMinimized = false;
   export let canBeMaximized = false;
   export let canBeResized = true;
-  export let xPosition = 0;
-  export let yPosition = 0;
-  export let maxWidth: number;
+  export let left = 0;
+  export let top = 0;
+  export let maxWidth = 0;
+
+  let windowDiv: HTMLElement;
 
   const onQuestionButtonClick = () => console.log("?");
   const onMinimizeButtonClick = () => console.log("_");
   const onMaximizeButtonClick = () => console.log("❒");
   const onCloseButtonClick = () => windowsHidden.update((wH: WindowsHiddenType) => ({ ...wH, login: true }));
 
-  console.log("test", document.getElementById("draggable"))
+  onMount(() => {
+    if (!left && !top) {
+      const rect = windowDiv.getBoundingClientRect();
+      top = rect.top + window.scrollY;
+      left = rect.left + window.scrollX;
+    }
+  })
 </script>
 
 <div
   class="border-2 border-gray-600 absolute"
   class:window-hidden={$windowsHidden.login}
-  class:window-center={!xPosition && !yPosition}
-  class:window-position={xPosition || yPosition}
+  class:window-center={!left && !top}
+  class:window-position={left || top}
   class:window-maxWidth={maxWidth}
-  style="--xPosition:{xPosition}; --yPosition:{yPosition}; --maxWidth:{maxWidth};"
+  style="--left:{left}; --top:{top}; --maxWidth:{maxWidth};"
+  bind:this={windowDiv}
 >
-  <div class="background-window-head flex justify-between">
-    <span class="text-white">{@html $t(title)}</span>
-    <div>
-      {#if hasQuestionButton}
-        <WindowButton on:click={onQuestionButtonClick}>?</WindowButton>
-      {/if}
-      {#if canBeMinimized}
-        <WindowButton on:click={onMinimizeButtonClick}>_</WindowButton>
-      {/if}
-      {#if canBeMaximized}
-        <WindowButton on:click={onMaximizeButtonClick}>❒</WindowButton>
-      {/if}
-      <WindowButton on:click={onCloseButtonClick}>X</WindowButton>
+  <DraggableItem bind:left bind:top>
+    <div class="background-window-head flex justify-between">
+      <span class="text-white">{@html $t(title)}</span>
+      <div>
+        {#if hasQuestionButton}
+          <WindowButton on:click={onQuestionButtonClick}>?</WindowButton>
+        {/if}
+        {#if canBeMinimized}
+          <WindowButton on:click={onMinimizeButtonClick}>_</WindowButton>
+        {/if}
+        {#if canBeMaximized}
+          <WindowButton on:click={onMaximizeButtonClick}>❒</WindowButton>
+        {/if}
+        <WindowButton on:click={onCloseButtonClick}>X</WindowButton>
+      </div>
     </div>
-  </div>
+  </DraggableItem>
   <slot />
 </div>
 
@@ -60,8 +74,8 @@
   }
   
   .window-position {
-    top: calc(var(--xPosition) * 1px);
-    left: calc(var(--yPosition) * 1px);
+    top: calc(var(--top) * 1px);
+    left: calc(var(--left) * 1px);
   }
 
   .window-maxWidth {
