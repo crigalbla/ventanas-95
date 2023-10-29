9b4a4de190014c0d01788e9f0adcc7a8802e484d
Cristian <cristian.gb.1995@hotmail.com>
2023-10-29 18:14:32 +0100

    npm run lint:fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,27 +3,27 @@ type CustomMouseEvent = MouseEvent & {
 }
 
 export const mouseOutOfScreen = (e: MouseEvent) => {
-  const { clientX, clientY } = e
-  const windowWidth = window.innerWidth
-  const windowHeight = window.innerHeight
-  if (clientX < 0 || clientX > windowWidth || clientY < 0 || clientY > windowHeight) return true
+	const { clientX, clientY } = e
+	const windowWidth = window.innerWidth
+	const windowHeight = window.innerHeight
+	if (clientX < 0 || clientX > windowWidth || clientY < 0 || clientY > windowHeight) return true
 
-  return false
+	return false
 }
 
 export const freezeCurrentCursor = (e: MouseEvent) => {
-  const html = document.querySelector("html") as HTMLElement
-  const body = document.querySelector("body") as HTMLElement
-  const currentStyle = window.getComputedStyle((e as CustomMouseEvent).toElement)
-  const cursor = currentStyle.cursor
+	const html = document.querySelector("html") as HTMLElement
+	const body = document.querySelector("body") as HTMLElement
+	const currentStyle = window.getComputedStyle((e as CustomMouseEvent).toElement)
+	const cursor = currentStyle.cursor
 
-  html.style.cursor = cursor
-  body.style.pointerEvents = "none"
+	html.style.cursor = cursor
+	body.style.pointerEvents = "none"
 }
 
 export const unfreezeCurrentCursor = () => {
-  const html = document.querySelector("html") as HTMLElement
-  const body = document.querySelector("body") as HTMLElement
-  html.style.cursor = ""
-  body.style.pointerEvents = ""
+	const html = document.querySelector("html") as HTMLElement
+	const body = document.querySelector("body") as HTMLElement
+	html.style.cursor = ""
+	body.style.pointerEvents = ""
 }
