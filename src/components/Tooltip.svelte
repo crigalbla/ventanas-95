f77f44fd8903e5de656f3ce7e959c789a6fc332a
Cristian <cristian.gb.1995@hotmail.com>
2024-02-28 12:27:12 +0100

    tooltip shadow added in login

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -7,12 +7,16 @@
 
   let isTooltipReady = false
   let tooltioRef: HTMLElement
+  let shadowWidth = 0
+  let shadowHeight = 0
 
   onMount(() => {
   	const checkIfTooltipFitsOnScreen = () => {
   		const rect = tooltioRef.getBoundingClientRect()
   		const isOutsideX = rect.right > window.innerWidth
   		const isOutsideY = rect.bottom > window.innerHeight
+  		shadowWidth = rect.width
+  		shadowHeight = rect.height
 
   		if (isOutsideX) left = left - rect.width
   		if (isOutsideY) top = window.innerHeight - rect.height
@@ -32,6 +36,11 @@
 >
   {$t("tooltip.text")}
 </section>
+<div
+  class="shadow"
+  class:not-ready={!isTooltipReady}
+  style="--top:{top + 20}; --left:{left + 20}; --shadowWidth:{shadowWidth}; --shadowHeight:{shadowHeight};"
+/>
 
 <style>
   .not-ready {
@@ -46,9 +55,20 @@
     top: calc(var(--top) * 1px);
     left: calc(var(--left) * 1px);
     min-width: 150px;
+    max-width: 440px;
     margin: 10px;
     background: rgb(255, 255, 245);
     border: 1px solid black;
+    z-index: 501;
+  }
+
+  .shadow {
+    position: absolute;
+    top: calc(var(--top) * 1px);
+    left: calc(var(--left) * 1px);
+    width: calc(var(--shadowWidth) * 1px);
+    height: calc(var(--shadowHeight) * 1px);
     z-index: 500;
+    background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="4" height="4"><rect x="0" y="0" width="5" height="5" fill="rgba(0, 0, 0, 0.2)" /><line x1="1" y1="5" x2="5" y2="0" stroke="rgba(0, 0, 0, 0.2)" stroke-width="1" /></svg>');
   }
 </style>
